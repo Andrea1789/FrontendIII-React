85d2c600074e920effac23803e9bddcb4036d0d9
Andrea Agudelo Cardona <andreaagudelocardona789@gmail.com>
2023-03-07 10:26:31 -0500

    Add files via upload
    Formulario tabla de materias con promedio de estudiantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,106 @@
-import { useState } from 'react'
-import './App.css'
-import { Form } from './modules/Form'
+import React, { useState } from "react";
+import Table from "./Table";
 
-function App() {
-  
+const Form = () => {
+  const [subjects, setSubjects] = useState([
+    {
+      id: 11,
+      value: "database",
+      careerName: "Base de Datos",
+      numOfStudents: 0,
+      sum: 0,
+      qualifiable: true,
+    },
+    {
+      id: 12,
+      value: "frontend",
+      careerName: "Desarrollo Frontend",
+      numOfStudents: 0,
+      sum: 0,
+      qualifiable: true,
+    },
+    {
+      id: 13,
+      value: "backend",
+      careerName: "Desarrollo Backend",
+      numOfStudents: 0,
+      sum: 0,
+      qualifiable: true,
+    },
+    {
+      id: 14,
+      value: "devops",
+      careerName: "Devops",
+      numOfStudents: 0,
+      sum: 0,
+      qualifiable: false,
+    },
+  ]);
+  const [career, setCareer] = useState("");
+  const [error, setError] = useState("");
+  const [qualification, setQualification] = useState(null);
+
+  const handleSelect = (e) => {
+    e.preventDefault();
+    setError("");
+    const careerValue = e.target.value;
+    const isCareerQualifiable = subjects.some(
+      (o) => o.value === careerValue && o.qualifiable === true
+    );
+    if (!isCareerQualifiable) {
+      setError("El curso seleccionado no es válido");
+    } else {
+      setCareer(careerValue);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const sujectIndex = subjects.findIndex((o) => o.value === career);
+    if (subjectIndex !== -1) {
+      const newList = subjects.map((item) => {
+        if (item === subjects[subjectIndex]) {
+          item.sum += qualification;
+          item.numOfStudents++;
+        }
+        return item;
+      });
+      setCareers(newList);
+    }
+  };
+
   return (
-    <>
-    <Form/>
-    </>
-  )
-}
+    <div className="container">
+      <h1>Promedio de estudiantes por carrera</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="container_input">
+          <select defaultValue="default" onChange={handleSelect}>
+            <option value="default" disabled hidden>
+              Seleccione una carrera
+            </option>
+            {careers.map((item) => {
+              return (
+                <option key={`option-key-${item.id}`} value={item.value}>
+                  {item.careerName}
+                </option>
+              );
+            })}
+          </select>
+          <input
+            type="number"
+            min="0"
+            max="10"
+            step="1"
+            onChange={(e) => setQualification(Number(e.target.value))}
+            defaultValue={"0"}
+          />
+        </div>
+        {error.length > 0 && <p>{error}</p>}
+        <input disabled={error.length > 0} type="submit" value="Salvar" />
+      </form>
+      <Table list={careers} />
+    </div>
+  );
+};
 
-export default App
+export default Form;
